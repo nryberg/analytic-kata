a95a9025ab290ab217fd6a9e40c6e37038cbf970
Nick Ryberg <nryberg@gmail.com>
2015-03-21 23:24:11 -0500

    Built out basis of line sampling

--- toolsets/go/go-random-lines.go
+++ toolsets/go/go-random-lines.go
@@ -14,10 +14,25 @@ import (
 //func simple_sample(rdr *io.Reader) {
 //}
 
+func fetchLine(f *os.File, line int) string {
+	count := 0
+	s := bufio.NewScanner(f)
+	var output string
+	for s.Scan() {
+
+		if count == line {
+			output = s.Text()
+		}
+		count += 1
+	}
+	return output
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	infile := "/users/nick/data/financial_insitition/all_2012/all_2012.csv"
 	outfile := "../samples/banks.csv"
+	sample_size := 10
 	//rnd := rand.Intn(10)
 	//fmt.Println(rnd)
 	fmt.Println(infile)
@@ -28,22 +43,32 @@ func main() {
 		os.Exit(1)
 	}
 
-	r := bufio.NewReader(f)
+	f_out, err := os.Create(outfile)
+	if err != nil {
+		fmt.Printf("error opening out file: %v\n", err)
+		os.Exit(1)
+	}
 
-	count, err := lineCounter(r)
-	fmt.Println("Lines : ", NumberToString(count, ','))
-	fmt.Println("Rand: ", NumberToString(int(rand.Int63n(int64(count))), ','))
-	/*
-		s, e := r.ReadString('\n')
-		for i := 1; i < 5; i++ {
-			fmt.Println(s)
-			s, e = r.ReadString('\n')
-			if e != nil {
-				fmt.Printf("error : %v\n", e)
-				os.Exit(1)
-			}
+	defer f_out.Close()
+
+	count, err := lineCounter(f)
+	f.Seek(0, 0)
+
+	// TODO: Create array of line numbers, sort the array and then
+	// simply pull those.  No need to seek.
+
+	for counter := 0; counter < sample_size; counter++ {
+		sample_line := int(rand.Int63n(int64(count)))
+		fmt.Println(counter, sample_line)
+		outline := fetchLine(f, sample_line)
+		f.Seek(0, 0)
+		_, err := f_out.WriteString(outline + "\n")
+		if err != nil {
+			fmt.Printf("error writing to file: %v\n", err)
+			os.Exit(1)
 		}
-	*/
+	}
+
 }
 
 func NumberToString(n int, sep rune) string {
@@ -80,34 +105,14 @@ func NumberToString(n int, sep rune) string {
 	return buff.String()
 }
 
-func lineCounter(r io.Reader) (int, error) {
+func lineCounter(f *os.File) (int, error) {
+	r := bufio.NewReader(f)
 	buf := make([]byte, 8196)
 	count := 0
 	lineSep := []byte{'\n'}
 
 	for {
 		c, err := r.Read(buf)
-		if err != nil && err != io.EOF {
-			return count, err
-		}
-
-		count += bytes.Count(buf[:c], lineSep)
-
-		if err == io.EOF {
-			break
-		}
-	}
-
-	return count, nil
-}
-
-func fetchLine(r io.Reader, line int) (string, error) {
-	buf := make([]byte, 8196)
-
-	lineSep := []byte{'\n'}
-
-	for count := 0; count < line; count++ {
-		str, err := r.ReadString(r, '\n')
 		if err != nil && err != io.EOF {
 			return count, err
 		}

bcfcc554ff24634422b4b71be8bb41c6df6f8503
Nick Ryberg <nryberg@gmail.com>
2016-01-04 07:36:54 -0600

    Uses modeling in YAML to generate correct struct
    Unmarshaling is hard if you're clueless about the desired format.  Fake it till you make it!

--- data/generate/data_generator.go
+++ data/generate/data_generator.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"fmt"
-	_ "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 )
 
+type Entry struct {
+	Fruit
+}
 type Fruit struct {
+	Name        string
+	Weight      float32
+	Measure     string
+	Price       float32
+	GrossWeight float32 `yaml:"gross_weight"`
 }
 
 func main() {
+	entry := Entry{}
 	dat, err := ioutil.ReadFile("./data_template.yaml")
 	if err != nil {
 		log.Println("File Read error: ", err)
 	}
-	fmt.Print(string(dat))
+	err = yaml.Unmarshal(dat, &entry)
+	if err != nil {
+		log.Println("File Read error: ", err)
+	}
+	fmt.Println(entry.Name)
+
+	fruit := Fruit{}
+	fruit.Name = "Fred"
+	fruit.Weight = 32
+	fruit.Measure = "Weight"
+
+	entries := []Entry{}
+	
+	entries = append(entries, 
+	entry.Fruit = fruit
+
+	log.Println("Entry:", entry)
+	d, err := yaml.Marshal(&entry)
+	if err != nil {
+		log.Println("Error: ", err)
+	}
+
+	fmt.Println(string(d))
 }

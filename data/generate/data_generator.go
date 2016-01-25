83fb2343a9ad34436cb19048a1546cad86d93a2d
Nick Ryberg <nryberg@gmail.com>
2016-01-25 16:37:34 -0600

    Moves to csv based base data files

--- data/generate/data_generator.go
+++ data/generate/data_generator.go
@@ -19,32 +19,34 @@ type Fruit struct {
 }
 
 func main() {
-	entry := Entry{}
+	entries := []Fruit{}
 	dat, err := ioutil.ReadFile("./data_template.yaml")
 	if err != nil {
 		log.Println("File Read error: ", err)
 	}
-	err = yaml.Unmarshal(dat, &entry)
+	err = yaml.Unmarshal(dat, &entries)
 	if err != nil {
 		log.Println("File Read error: ", err)
 	}
-	fmt.Println(entry.Name)
+	log.Println("Entry count: ", len(entries))
 
-	fruit := Fruit{}
-	fruit.Name = "Fred"
-	fruit.Weight = 32
-	fruit.Measure = "Weight"
+	fmt.Println(entries[2])
+	/*
+		entries = append(entries, entry)
 
-	entries := []Entry{}
-	
-	entries = append(entries, 
-	entry.Fruit = fruit
+		fruit := Fruit{}
+		fruit.Name = "Fred"
+		fruit.Weight = 32
+		fruit.Measure = "Weight"
 
-	log.Println("Entry:", entry)
-	d, err := yaml.Marshal(&entry)
-	if err != nil {
-		log.Println("Error: ", err)
-	}
+		entry.Fruit = fruit
 
-	fmt.Println(string(d))
+		entries = append(entries, entry)
+		d, err := yaml.Marshal(&entries)
+		if err != nil {
+			log.Println("Error: ", err)
+		}
+
+		fmt.Println(string(d))
+	*/
 }

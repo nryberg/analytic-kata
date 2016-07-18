538422453d9173d0c2c4fc8e4a58393074350e98
Nick Ryberg <nryberg@gmail.com>
2016-07-17 22:38:06 -0500

    Trying different themes

--- website/hugme.bash
+++ website/hugme.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
+#hugo server --theme=hugo-bootstrap-premium --buildDrafts
 #hugo server --theme=hugo-theme-bootie-docs --buildDrafts
-hugo server --theme=purehugo --buildDrafts
+#hugo server --theme=hugo-material-docs --buildDrafts
+hugo server --theme=reggie --buildDrafts

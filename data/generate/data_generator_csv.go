5113cd63404441d029c461c5162d96acf9ae68a3
Nick Ryberg <nryberg@gmail.com>
2016-10-05 21:48:55 -0500

    Incorporates yaml config into generator

--- data/generate/data_generator_csv.go
+++ data/generate/data_generator_csv.go
@@ -4,9 +4,23 @@ import (
 	//"fmt"
 	// "gopkg.in/yaml.v2"
 	"encoding/csv"
+	"io/ioutil"
 	"log"
 	"os"
+
+	yaml "gopkg.in/yaml.v1"
 )
+
+type Config struct {
+	facts struct {
+		description  string
+		name         string
+		dateScale    string
+		quantityLow  int
+		quantityHigh int
+	}
+	dimensions []string
+}
 
 type entry struct {
 	fruit
@@ -20,14 +34,29 @@ type fruit struct {
 	GrossWeight float32 `yaml:"gross_weight"`
 }
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
 
-	f, err := os.Open("./base_fruit.csv")
+	f, err := os.Open("./Apples_and_Oranges/fruit.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+
+	configFile, err := ioutil.ReadFile("./Apples_and_Oranges/config.yaml")
+	check(err)
+
+	err = yaml.Unmarshal(configFile, &config)
+	check(err)
 
 	//	dat, err := ioutil.ReadFile("./base_fruit.csv")
-	if err != nil {
-		log.Println("File Read error: ", err)
-	}
+
 	defer f.Close()
 
 	lines, err := csv.NewReader(f).ReadAll()
